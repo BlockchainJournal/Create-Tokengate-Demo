40e7cea1b84480a46a4ed1038c9051b01c1076de
Bob Reselman <bob@cogarttech.com>
2023-11-10 08:01:28 -0800

    Updated script/verifyAddressHasToken.js to get data automagically

--- hardhat/scripts/verifyAddressHasToken.js
+++ hardhat/scripts/verifyAddressHasToken.js
@@ -1,4 +1,6 @@
 const { ethers } = require("hardhat");
+const {join} = require("path");
+const fs = require("fs");
 
 /*
 Command line example:
@@ -8,36 +10,27 @@ npx hardhat run scripts/verifyAddressHasToken.js --network sepolia
  */
 
 async function verifyAddressHasToken() {
-
-    // Replace with the contract address of the ERC-721 contract
-    const contractAddress = "0x3039Cc64a6157450242B4bC9522Bea2b7D45E359";
-
-    // Replace with the token ID you want to verify
-    const tokenIdToVerify = 1; // Example token ID
+    const directoryPath = join(__dirname, './data');
+    const filePath = join(directoryPath, 'dilty-addresses.json');
+    const json = fs.readFileSync(filePath, 'utf8');
+    const contractAddress = JSON.parse(json).diltyAddress;
+    const contractABI = require("./data/dilty-abi.json");
 
     // Replace with the account address you want to check
     const addressToCheck = "0x9e4aF6FDa84260f957Ff65E1EE447E522C5E0e27"; // Replace with the address you want to check
 
-    const contractABI = require("./dilty-abi.json");
-
     // Connect to the ERC-721 contract
     const contract = await ethers.getContractAt(contractABI, contractAddress);
 
-    // Use the balanceOf function to check if the account owns the specified token
-    const balance = await contract.balanceOf(addressToCheck);
+    // Use the hasAddress function to check if the address owns the specified token
+    const result = await contract.hasAddress(addressToCheck);
 
 
-    if (balance === 0) {
-        console.log(`Account ${addressToCheck} does not own any tokens.`);
-        return;
-    }
-
-    const owner = await contract.ownerOf(tokenIdToVerify);
-
-    if (owner === addressToCheck) {
-        console.log(`Account ${addressToCheck} owns the token with ID ${tokenIdToVerify}.`);
-    } else {
-        console.log(`Account ${addressToCheck} does not own the token with ID ${tokenIdToVerify}.`);
+    if (result) {
+        const tokenId = await contract.getAddressTokenId(addressToCheck);
+        console.log(`Account ${addressToCheck} owns the token with ID ${tokenId}.`);
+    }else{
+        console.log(`Account ${addressToCheck} does not own a token.`);
     }
 }
 
